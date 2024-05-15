23ec3b7673b6c942cae648c3017efcc9fdebc6c8
Adam Vegvari <brokenskijas9@gmail.com>
2024-05-15 17:26:09 +0200

    navbar & header is done, with responsive design

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Outlet } from "react-router-dom"
+
+//components
+import HeaderComponent from "./components/HeaderComponent"
+import NavbarComponent from "./components/NavbarComponent"
+
+
+function App() {
+
+  return (
+    <>
+      <HeaderComponent/>
+      <NavbarComponent/>
+      <Outlet/>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+//pages
+import HomePage from './pages/HomePage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      }
+    ]
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
